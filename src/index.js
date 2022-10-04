ae39c1b7e1231318ffe0ba05176d140af6c6c5e2
David <david.programming@protonmail.com>
2022-10-04 06:56:17 +0200

    feat: on search apply weather from api

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './styles/main.scss';
+
+var weather = {
+  apiKey: '',
+  fetchWeather(city) {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&units=metric&appid=' +
+        this.apiKey
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return this.displayWeather(data);
+      });
+  },
+  displayWeather(data) {
+    const name = data.name;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const wind = data.wind.speed;
+    document.querySelector('#weather-heading').innerText = 'Weather in ' + name;
+    document.querySelector('#weather-input').setAttribute('placeholder', name);
+    document.querySelector('#weather-degree').innerText =
+      Math.round(temp) + '°C';
+    document.querySelector('#weather-icon').src =
+      'http://openweathermap.org/img/wn/' + icon + '.png';
+    document.querySelector('#weather-description').innerText = description;
+    document.querySelector('#humidity-description').innerText = humidity + ' %';
+    document.querySelector('#wind-description').innerText =
+      (3.6 * wind).toFixed(2) + ' km/h';
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name.toLowerCase() + "')";
+  },
+  search: function search() {
+    this.fetchWeather(document.querySelector('#weather-input').value);
+  },
+};
+// ========================= EVENT LISTENER =========================
+document
+  .querySelector('#weather-input')
+  .addEventListener('keyup', function (e) {
+    if (e.key == 'Enter') weather.search();
+  });
